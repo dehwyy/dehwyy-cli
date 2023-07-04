5da7a45c456056afcf429758e7e5fecb3a665ead
dehwyy <dehwyy@yandex.ru>
2023-07-04 15:26:17 +0300

    upd: new cmd command - Use

---
+++ cmd/use.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	logger = log.New(os.Stdout, "", 0)
+	useCmd = &cobra.Command{
+		Use: "use [command]",
+		Short: "Use is a shortcut to run several commands by using just one",
+		Long: "Use is a shortcut to run several commands by using just one",
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, stringsArray[]string) {
+			for _, s := range stringsArray {
+				args:= strings.Split(s, " ")
+				e := exec.Command(args[0],args[1:]...).Start()
+				if e != nil {
+					logger.Fatalf("Error occured: %v", e)
+				}
+			}
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(useCmd)
+}
